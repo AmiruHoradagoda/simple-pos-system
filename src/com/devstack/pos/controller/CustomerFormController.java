e1bc38b0397d5d086e9d7bfb1c2ea95f35f3a2cd
AmiruHoradagoda <139224955+AmiruHoradagoda@users.noreply.github.com>
2025-01-13 01:26:13 +0530

    - Implement Customer save process - create CoustomerDto

--- src/com/devstack/pos/controller/CustomerFormController.java
+++ src/com/devstack/pos/controller/CustomerFormController.java
@@ -1,12 +1,16 @@
 package com.devstack.pos.controller;
 
+import com.devstack.pos.dao.DatabaseAccessCode;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+
+import java.sql.SQLException;
 
 public class CustomerFormController {
     public JFXTextField txtEmail;
@@ -25,7 +29,35 @@ public class CustomerFormController {
     public AnchorPane context;
 
     public void btnSaveUpdateOnAction(ActionEvent actionEvent) {
+        try {
+            if (
+                    DatabaseAccessCode.createCustomer(
+                            txtEmail.getText(),
+                            txtName.getText(),
+                            txtContact.getText(),
+                            Double.parseDouble(txtSalary.getText())
+                    )
+            ) {
+                new Alert(Alert.AlertType.CONFIRMATION, "Customer Saved!").show();
+                clearFilds();
+            } else {
+                new Alert(Alert.AlertType.WARNING, "Tri Again!").show();
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.WARNING, "Tri Again!").show();
+
+        }
+
     }
+
+    private void clearFilds() {
+        txtEmail.clear();
+        txtName.clear();
+        txtContact.clear();
+        txtSalary.clear();
+    }
+
 
     public void btnBackToHomeOnAction(ActionEvent actionEvent) {
 
--- src/com/devstack/pos/dao/DatabaseAccessCode.java
+++ src/com/devstack/pos/dao/DatabaseAccessCode.java
@@ -1,9 +1,13 @@
 package com.devstack.pos.dao;
 
+import com.devstack.pos.dto.CustomerDto;
 import com.devstack.pos.dto.UserDto;
 import com.devstack.pos.util.PasswordManager;
 
 import java.sql.*;
+import java.util.List;
+
+//====== <User Management>=======
 
 public class DatabaseAccessCode {
     public static boolean createUser(
@@ -16,18 +20,18 @@ public class DatabaseAccessCode {
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
         preparedStatement.setString(1, email);
         preparedStatement.setString(2, PasswordManager.encryptPassword(password));
-        return preparedStatement.executeUpdate()>0;
+        return preparedStatement.executeUpdate() > 0;
     }
 
-    public static UserDto findUser(String email) throws ClassNotFoundException, SQLException{
+    public static UserDto findUser(String email) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/robotikka",
                 "root", "1234");
         String sql = "SELECT * FROM user WHERE email=?";
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        preparedStatement.setString(1,email);
+        preparedStatement.setString(1, email);
         ResultSet resultSet = preparedStatement.executeQuery();
-        if (resultSet.next()){
+        if (resultSet.next()) {
             return new UserDto(
                     resultSet.getString(1),
                     resultSet.getString(2)
@@ -35,5 +39,60 @@ public class DatabaseAccessCode {
         }
         return null;
     }
+//====== </User Management>=======
+
+    //====== Customer Management =======
+    public static boolean createCustomer(
+            String email,
+            String name,
+            String contact,
+            double salary
+    ) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/robotikka",
+                "root", "1234");
+        String sql = "INSERT INTO customer VALUES ( ?,?,?,? )";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, email);
+        preparedStatement.setString(2, name);
+        preparedStatement.setString(3, contact);
+        preparedStatement.setDouble(4, salary);
+        return preparedStatement.executeUpdate() > 0;
+    }
+
+    public static boolean updateCustomer(
+            String email,
+            String name,
+            String contact,
+            double salary
+    ) {
+        return false;
+    }
+
+    public static CustomerDto findCustomer(
+            String email
+    ) {
+        return null;
+    }
+
+    public static boolean deleteCustomer(
+            String email
+    ) {
+        return false;
+    }
+
+
+    public static List<CustomerDto> findAllCustomers(
+
+    ) {
+        return null;
+    }
+
+    public static CustomerDto searchCustomers(
+            String searchText
+    ) {
+        return null;
+    }
+
 
 }
