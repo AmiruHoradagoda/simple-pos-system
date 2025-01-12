02ca2e8521723e13623486bde14efaa121f6b31f
AmiruHoradagoda <139224955+AmiruHoradagoda@users.noreply.github.com>
2025-01-12 23:47:17 +0530

    create CustomerForm and its Controller

---
+++ src/com/devstack/pos/controller/CustomerFormController.java
@@ -0,0 +1,27 @@
+package com.devstack.pos.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class CustomerFormController {
+    public JFXTextField txtEmail;
+    public JFXTextField txtName;
+    public JFXTextField txtContact;
+    public JFXTextField txtSalary;
+    public JFXButton btnSaveUpdate;
+    public TableView tbl;
+    public TableColumn colId;
+    public TableColumn colEmail;
+    public TableColumn colName;
+    public TableColumn colContact;
+    public TableColumn colSalary;
+    public TableColumn colOperate;
+    public TextField txtSearch;
+
+    public void btnSaveUpdateOnAction(ActionEvent actionEvent) {
+    }
+}
