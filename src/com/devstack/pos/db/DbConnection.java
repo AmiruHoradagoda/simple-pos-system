c4161098e57f53bf1ce84db9c439dbf4a26b4fcb
AmiruHoradagoda <amirumithsara1234@gmail.com>
2025-01-26 00:57:27 +0530

    Added DbConnection class for database connectivity implemented using the Singleton design pattern

--- src/com/devstack/pos/db/DbConnection.java
+++ src/com/devstack/pos/db/DbConnection.java
@@ -1,2 +1,25 @@
-package com.devstack.pos.db;public class DbConnection {
+package com.devstack.pos.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+// Using Singleton Design pattern
+public class DbConnection {
+    private static DbConnection dbconnection;
+    private Connection connection;
+
+    private DbConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/robotikka",
+                "root", "1234");
+    }
+
+    public static DbConnection getInstance() throws SQLException, ClassNotFoundException {
+        return (dbconnection == null) ? dbconnection = new DbConnection() : dbconnection;
+    }
+    public Connection getConnection(){
+        return connection;
+    }
+
 }
