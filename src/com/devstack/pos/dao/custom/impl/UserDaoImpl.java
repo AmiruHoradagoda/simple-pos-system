70e5e3734aa2d3db8452f552dabb15e513f97d1c
AmiruHoradagoda <amirumithsara1234@gmail.com>
2025-01-28 10:16:30 +0530

    Error fix: UserDaoImpl methods to handle User entity operations and update SQL statements

--- src/com/devstack/pos/dao/custom/impl/UserDaoImpl.java
+++ src/com/devstack/pos/dao/custom/impl/UserDaoImpl.java
@@ -1,33 +1,44 @@
 package com.devstack.pos.dao.custom.impl;
 
 import com.devstack.pos.dao.custom.UserDao;
+import com.devstack.pos.db.DbConnection;
 import com.devstack.pos.entity.User;
+import com.devstack.pos.util.PasswordManager;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDaoImpl implements UserDao {
+
+
     @Override
-    public boolean saveProduct(User customer) {
+    public boolean saveUser(User user) throws SQLException, ClassNotFoundException {
+        String sql = "INSERT INTO user VALUES ( ?,? )";
+        PreparedStatement preparedStatement = DbConnection.getInstance().getConnection().prepareStatement(sql);
+        preparedStatement.setString(1, user.getEmail());
+        preparedStatement.setString(2, PasswordManager.encryptPassword(user.getPassword()));
+        return preparedStatement.executeUpdate() > 0;
+
+    }
+
+    @Override
+    public boolean updateUser(User user) {
         return false;
     }
 
     @Override
-    public boolean updateProduct(User customer) {
+    public boolean deleteUser(String email) {
         return false;
     }
 
     @Override
-    public boolean deleteProduct(String email) {
-        return false;
-    }
-
-    @Override
-    public User findProduct(String email) {
+    public User findUser(String email) {
         return null;
     }
 
     @Override
-    public List<User> findAllProduct() {
+    public List<User> findAllUser() {
         return null;
     }
 }
