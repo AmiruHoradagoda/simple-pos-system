a051bcd3886f8fbc544c9dfd0073991879c65107
AmiruHoradagoda <amirumithsara1234@gmail.com>
2025-01-28 11:13:29 +0530

    Add exception handling to ProductDao and ProductDaoImpl methods for improved error management

--- src/com/devstack/pos/dao/custom/ProductDao.java
+++ src/com/devstack/pos/dao/custom/ProductDao.java
@@ -2,12 +2,17 @@ package com.devstack.pos.dao.custom;
 
 import com.devstack.pos.entity.Product;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface ProductDao {
-    public boolean saveProduct (Product customer);
-    public boolean updateProduct (Product customer);
-    public boolean deleteProduct (int code); //fix error
-    public Product findProduct (int code);//fix error
-    public List<Product> findAllProduct ();
+    public boolean saveProduct(Product product) throws SQLException, ClassNotFoundException;
+
+    public boolean updateProduct(Product product) throws SQLException, ClassNotFoundException;
+
+    public boolean deleteProduct(int code) throws SQLException, ClassNotFoundException; //fix error
+
+    public Product findProduct(int code) throws SQLException, ClassNotFoundException;//fix error
+
+    public List<Product> findAllProduct() throws SQLException, ClassNotFoundException;
 }
